d76aa01f8335b11427c3c6e87da7dcd60e40cfe4
Martin Griesler <e0968732@student.tuwien.ac.at>
2016-11-10 18:45:45 +0100

    renamed service added webdependencies

---
+++ quoteService/src/main/java/at/ac/tuwien/aic/group6/QuoteServiceApplication.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.aic.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteServiceApplication.class, args);
+	}
+}
