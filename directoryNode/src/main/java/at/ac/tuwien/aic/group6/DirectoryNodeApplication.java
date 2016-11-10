d528d3d2abcd7ab2c394339392409c205d27d059
Martin Griesler <e0968732@student.tuwien.ac.at>
2016-11-10 18:14:12 +0100

    initial commit

---
+++ directoryNode/src/main/java/at/ac/tuwien/aic/group6/DirectoryNodeApplication.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.aic.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectoryNodeApplication.class, args);
+	}
+}
